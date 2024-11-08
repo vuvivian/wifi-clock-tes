d8987c0471fb9fd0f2cb2f4eee6cc1930bc24893
wangjuan3@chanjet.com <wangjuan3@chanjet.com>
2024-11-08 17:57:10 +0800

    调整

--- README.md
+++ README.md
@@ -25,3 +25,15 @@ PyObjC 是一个 Python 库，它允许 Python 开发者调用 Objective-C 代�
 spawn函数用于异步地执行外部命令，并返回一个新的ChildProcess实例。
 
 与exec函数相比，spawn更加灵活，因为它允许直接从子进程的stdout和stderr流中读取数据，而不是在命令执行完毕后一次性获取全部输出。
+
+
+### Puppeteer
+
+Puppeteer 是一个 Node.js 库，它提供了一个高级 API 来控制 Chrome 或 Chromium 浏览器。它允许你通过 JavaScript 代码来自动化浏览器操作，例如：
+- 生成页面 PDF。
+- 抓取 SPA（单页应用）并生成预渲染内容（即“SSR”（服务器端渲染））。
+- 自动提交表单，进行 UI 测试，键盘输入等。
+- 创建一个时时更新的自动化测试环境。 使用最新的 JavaScript 和浏览器功能直接在最新版本的Chrome中执行测试。
+- 捕获网站的 timeline trace，用来帮助分析性能问题。
+- 测试浏览器扩展。
+
--- index.ts
+++ index.ts
@@ -9,44 +9,16 @@ const puppeteer = require('puppeteer');
 const wifi = require('node-wifi');
 const { spawn } = require('child_process');
 
-function getWifiList() {
-  return new Promise((resolve, reject) => {
-    const pythonProcess = spawn('/usr/bin/python3', ['wifi_scan.py']);
+// 1. 检索名称为【时光荏苒】的热点
+// 2. 五分钟内持续检索，如果没找到就一直找，5分钟后没热点，抛出异常
+// 3. 找到后 连接热点
+// 3. 连接成功后，打开网页 http://192.168.4.1/
+// 4. 输入用户名和密码
+// 5. 点击提交
+// 6. 等待一段时间以查看结果
+// 7. 退出
 
-    let output = '';
-    pythonProcess.stdout.on('data', (data) => {
-      output += data.toString();
-    });
-
-    pythonProcess.stderr.on('data', (data) => {
-      console.error(`Error: ${data}`);
-    });
-
-    pythonProcess.on('close', (code) => {
-      if (code !== 0) {
-        reject(new Error(`Python script exited with code ${code}`));
-      } else {
-        try {
-          const networks = JSON.parse(output);
-          resolve(networks);
-        } catch (e) {
-          reject(e);
-        }
-      }
-    });
-  });
-}
-
-  // 1. 检索名称为【时光荏苒】的热点
-  // 2. 五分钟内持续检索，如果没找到就一直找，5分钟后没热点，抛出异常
-  // 3. 找到后 连接热点
-  // 3. 连接成功后，打开网页 http://192.168.4.1/
-  // 4. 输入用户名和密码
-  // 5. 点击提交
-  // 6. 等待一段时间以查看结果
-  // 7. 退出
-
-  // 重复执行上述7步 直到强行结束程序
+// 重复执行上述7步 直到强行结束程序
 
 const ssid = '时光荏苒'; // 热点名称
 const username = '@Ruijie-sECF4'; // 替换为你的用户名
@@ -95,22 +67,49 @@ async function connectToWifi() {
             ]);
 
             // 6. 等待一段时间以查看结果
-            // await page.waitForTimeout(10000); // 等待5秒查看结果
-
-            // todo  判断结果进行打印 成功就退出从新开始
+            const shouldExit = await delayedExit(); // 等待5秒等待5秒查看结果
 
             // 7. 退出
-            const shouldExit = await delayedExit(); // 等待5秒
             if (shouldExit) {
               await browser.close();
               break; // 如果5秒已过，则退出循环
             }
-          
+             // todo  判断结果进行打印 成功就退出从新开始
+             // todo 日志打印
+             // todo 程序打包
         } else {
             console.log(`未找到热点: ${ssid}，重新尝试...`);
             await new Promise(resolve => setTimeout(resolve, 5000)); // 等待5秒再重试
         }
     }
+}
+
+function getWifiList() {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn('/usr/bin/python3', ['wifi_scan.py']);
+
+    let output = '';
+    pythonProcess.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+      console.error(`Error: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Python script exited with code ${code}`));
+      } else {
+        try {
+          const networks = JSON.parse(output);
+          resolve(networks);
+        } catch (e) {
+          reject(e);
+        }
+      }
+    });
+  });
 }
 
 (async () => {
